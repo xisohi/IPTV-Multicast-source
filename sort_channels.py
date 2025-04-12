a1b720044ab9cb3a2d1b84ef508a9ed314f287bb
xihi <xuhui0607@gmail.com>
2025-04-12 09:43:27 +0800

    update

--- sort_channels.py
+++ sort_channels.py
@@ -50,7 +50,7 @@ if content:
     sorted_content.extend(filtered_lines)
 
     # 保存到文件
-    with open("fanmingming_channels.m3u", "w") as f:
+    with open("live.txt", "w") as f:
         f.write("\n".join(sorted_content))
 else:
     print("未获取到内容，无法进行排序。")
